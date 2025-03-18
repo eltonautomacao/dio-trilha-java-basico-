07270805e46a9d1579ab11350f9bfeac8ddcd313
eltonautomacao <egaautomacao@gmail.com>
2025-03-18 19:45:38 -0300

    Projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor digite seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Por favor digite o numero da agencia:");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Por favor digite o número da conta;");
+        int conta = scanner.nextInt();
+
+        double saldo = 25;
+
+        System.out.println("Olá " + nome +", obrigado pr criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta +", seu saldo é " + saldo + " já está disponivel para saque.");
+      
+
     }
 }
